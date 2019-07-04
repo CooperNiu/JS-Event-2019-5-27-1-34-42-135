ff9720e9c61e2aea61f6b8ac5f9ebf2e34611cd4
CooperNiu <niuyanno.1@outlook.com>
2019-07-04 18:29:33 +0800

    Create result.js

---
+++ 1.alert/result.js
@@ -0,0 +1,27 @@
+ <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>事件监听</title>
+  </head>
+  <body>
+  
+  <ul id="no">
+      <li onclick="myfunc(1)">1</li>
+      <li onclick="myfunc(2)">2</li>
+      <li onclick="myfunc(3)">3</li>
+      <li  onclick="myfunc(4)">4</li>
+      <li onclick="myfunc(5)">5</li>
+      <li onclick="myfunc(6)">6</li>
+      <li onclick="myfunc(7)">7</li>
+  </ul>
+  
+  <script>
+     // write your code here 
+     // ......
+     function myfunc(x) {
+     	alert(x);
+     }
+  </script>
+  </body>
+  </html>
